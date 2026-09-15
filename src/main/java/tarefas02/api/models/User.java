a5ab2d98a79a3735e706e2fc37defaeaac4cefa3
Alexandre Silveira de Paula <silveiradepaula.alexandre@gmail.com>
2026-09-15 10:15:15 -0300

    Alterações no TAsk

--- src/main/java/tarefas02/api/models/User.java
+++ src/main/java/tarefas02/api/models/User.java
@@ -1,26 +1,24 @@
 package tarefas02.api.models;
 
-import java.util.Objects;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
-
-    public interface CreateUser {}
-    public interface UpdateUser {}
 
     public static final String TABLE_NAME = "user";
 
@@ -30,32 +28,33 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotNull(groups = CreateUser.class)
-    @NotBlank(groups = CreateUser.class)
-    @Size(groups = CreateUser.class, min = 2, max = 100)
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 100)
     private String username;
 
-    @JsonProperty(access = Access.WRITE_ONLY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull(groups = {CreateUser.class, UpdateUser.class})
-    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 60)
     private String password;
 
-    // Construtor vazio (exigido pelo JPA/Hibernate)
+    @OneToMany(mappedBy = "user")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<Task> tasks = new ArrayList<>();
+
     public User() {
     }
 
-    // Construtor completo
     public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
     }
 
-    // Getters e Setters
     public Long getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(Long id) {
@@ -63,7 +62,7 @@ public class User {
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
     }
 
     public void setUsername(String username) {
@@ -71,35 +70,35 @@ public class User {
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (!(obj instanceof User)) return false;
-
-        User other = (User) obj;
-        if (this.id == null) {
-            if (other.id != null) return false;
-        } else if (!this.id.equals(other.id)) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
             return false;
-        }
-
-        return Objects.equals(this.username, other.username) 
-            && Objects.equals(this.password, other.password);
+        User other = (User) obj;
+        return Objects.equals(this.id, other.id)
+                && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
     }
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
+        return Objects.hash(this.id, this.username, this.password);
     }
 }
