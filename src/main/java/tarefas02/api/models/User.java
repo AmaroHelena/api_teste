796876884e5b5deac7fc24eeff429bd3ccd1c16c
Helena <helenaa793@gmail.com>
2026-09-16 15:54:15 -0300

    Criaçao do taskservice

--- src/main/java/tarefas02/api/models/User.java
+++ src/main/java/tarefas02/api/models/User.java
@@ -1,47 +1,56 @@
 package tarefas02.api.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = Task.TABLE_NAME)
-public class Task {
+@Table(name = User.TABLE_NAME)
+public class User {
 
-    public static final String TABLE_NAME = "task";
+    public static final String TABLE_NAME = "user";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false, updatable = false)
-    private User user;
-
-    @Column(name = "description", length = 255, nullable = false)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull
     @NotBlank
-    @Size(min = 1, max = 255)
-    private String description;
+    @Size(min = 2, max = 100)
+    private String username;
 
-    public Task() {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 60)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<Task> tasks = new ArrayList<>();
+
+    public User() {
     }
 
-    public Task(Long id, User user, String description) {
+    public User(Long id, String username, String password) {
         this.id = id;
-        this.user = user;
-        this.description = description;
+        this.username = username;
+        this.password = password;
     }
 
     public Long getId() {
@@ -52,39 +61,44 @@ public class Task {
         this.id = id;
     }
 
-    public User getUser() {
-        return user;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getDescription() {
-        return description;
+    public String getPassword() {
+        return password;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-
         if (obj == null || getClass() != obj.getClass())
             return false;
-
-        Task other = (Task) obj;
-
+        User other = (User) obj;
         return Objects.equals(this.id, other.id)
-                && Objects.equals(this.user, other.user)
-                && Objects.equals(this.description, other.description);
+                && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.user, this.description);
+        return Objects.hash(this.id, this.username, this.password);
     }
 }
--- src/main/java/tarefas02/api/services/TaskService.java
+++ src/main/java/tarefas02/api/services/TaskService.java
@@ -1,5 +1,7 @@
+//Pacote onde esta esta classe
 package tarefas02.api.services;
 
+//Importa list da biblioteca padrão do java para manipular coleção de objto
 import java.util.List;
 import java.util.Optional;
 
@@ -11,49 +13,86 @@ import tarefas02.api.models.Task;
 import tarefas02.api.models.User;
 import tarefas02.api.repositories.TaskRepository;
 
+//Anotação que indica as regras para a String reponsavel
 @Service
 public class TaskService {
 
+
+    //Injeta automaticamente a instancia do TaskRepositorio gerencia pelo Spring
     @Autowired
     private TaskRepository taskRepository;
 
+    //Injeta automaticamente a instancia do UserRepositorio gerencia pelo Spring
     @Autowired
     private UserService userService;
 
+    //Metodo para buscar task apatir do id
     public Task findById(Long id) {
+        //Executa a busca no banco, retorna um Optional contendo(ou nao) a Task
         Optional<Task> task = this.taskRepository.findById(id);
+
+        // Se a tarefa existir. retorna o objto,se estiver vazio,lança um runTimeException
         return task.orElseThrow(() -> new RuntimeException(
             "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()
         ));
     }
 
+    //Metodo para buscar todas as tarefas vinculadas a um determindo usuario
     public List<Task> findAllByUserId(Long userId) {
+
+        //Chama o UserService para garantir que o usuario exista no banco (lança escuçao se não existir)
         this.userService.findById(userId);
+
+        //Executa a busca a lista de tarefas
         List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+
+        //Retona a lista de tarefas
         return tasks;
     }
 
+    //Garante que criaçao ocorra dentro de uma  transação de banco de dados(rolback automatico se falhar)
     @Transactional
     public Task create(Task obj) {
+        //Valida se o usuario informado no objeto realmete existe no banco a recupera seus dados
         User user = this.userService.findById(obj.getUser().getId());
+
+        //Definie o id como null para garantir que o jpa realize um inserção(insert) e não uma atualização
         obj.setId(null);
+        //Associa a entidade User completa a validadada a tarefa
         obj.setUser(user);
+        // Salva a nva tarefa no banco de dados e atualiza"obj" com o id gerado
         obj = this.taskRepository.save(obj);
+        //Retorna a tarefa salva 
         return obj;
     }
-
+    //Garante que a atualização dentro de transação isolada no banco
     @Transactional
     public Task update(Task obj) {
+
+        //Reaproveita o findByID para verficar se atarefa a ser atualizada exista realmente
         Task newObj = findById(obj.getId());
+
+        //Atualiza apenas o campo descricao do objeto persistido com o novo valo
         newObj.setDescription(obj.getDescription());
+
+        //Salva a alteração no banco de dados e retorna o objeto atualização
         return this.taskRepository.save(newObj);
     }
 
+     //Metodo para deletar uma tarefa pelo id
     public void delete(Long id) {
+
+        //Verifica se a tarefa existe antes de tentar deletar
         findById(id);
+
         try {
+
+            //Solicita a remoção da tarefa no banco de dados pelo id
             this.taskRepository.deleteById(id);
+
         } catch (Exception e) {
+
+            //Capctura exeçoes (como violaçoes de chave estrangeira e lança uma mensagem amigavel)
             throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
         }
     }
